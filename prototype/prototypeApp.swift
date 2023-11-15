10c189c745f27fbe4e57e0a477697cf3002827ad
Rob <rob.edward.henry@gmail.com>
2023-11-15 13:30:11 +0800

    Initial Commit

---
+++ prototype/prototypeApp.swift
@@ -0,0 +1,20 @@
+//
+//  prototypeApp.swift
+//  prototype
+//
+//  Created by Robert Henry on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct prototypeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
